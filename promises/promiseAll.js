f4fed45c5d2cf0b7efba65a6ef339f6432e1ad95
Benison-G <sachinlg60@gmail.com>
2026-07-07 14:34:31 +0530

    Promise.All()

---
+++ promises/promiseAll.js
@@ -0,0 +1,29 @@
+const promise1 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Resolved promise 1")
+        }, 1000)
+    })
+}
+
+const promise2 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Resolved promise 2")
+        }, 1000)
+    })
+}
+
+const promise3 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Resolved promise 3")
+        }, 1000)
+    })
+}
+
+Promise.all([promise1(), promise2(), promise3()]).then((res) => {
+    console.log(res)
+}).catch((err) => {
+    console.log(err);
+})
