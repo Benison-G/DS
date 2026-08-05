e5d21f98186c106be284db260274ff53ac5fed6a
Benison-G <sachinlg60@gmail.com>
2026-08-05 21:38:28 +0530

    Create insertionSort.js

---
+++ insertionSort.js
@@ -0,0 +1,18 @@
+const insertionSort = (arr) => {
+    const n = arr.length;
+    for(let i = 1; i < n; i++) {
+        let curr = arr[i];
+        let prev = i - 1;
+
+        while(arr[prev] > curr && prev >= 0) {
+            arr[prev + 1] = arr[prev];
+            prev--;
+        } 
+
+        arr[prev + 1] = curr
+    }
+
+    return arr;
+}
+
+console.log(insertionSort([5,12,7,3,1,9]));
