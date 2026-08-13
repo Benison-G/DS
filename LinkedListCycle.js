7771ffbbcac40f93d30330f98174cf21a2d9c189
Benison-G <sachinlg60@gmail.com>
2026-08-13 18:11:22 +0530

    Update LinkedListCycle.js

--- LinkedListCycle.js
+++ LinkedListCycle.js
@@ -6,6 +6,9 @@
 // We can either user Set() -> add each node and return true if you find the next node in the set.
 // But we can solve it using fast and slow pointer.
 
+// This is floyd's algorithm - cycle goes with 2 pointers, one will be slow and other will be fast.
+// At some path of their cycle both will meet at same point.
+
 
 const hasCycle = (head) => {
     let slow = head;
