19a9022b2fbc02fe286998da42f67ef04cd7138c
Benison-G <sachinlg60@gmail.com>
2026-08-26 13:50:10 +0530

    Create validPalindrome_II.js

---
+++ strings/validPalindrome_II.js
@@ -0,0 +1,21 @@
+const isValidPalindrom = (s) => {
+    s = s.toLowerCase();
+    let i = 0;
+    let j = s.length - 1;
+    while(i < j) {
+        if (!s[i].match(/[a-z0-9]/i)) {
+            ++i;
+        } else if (!s[j].match(/[a-z0-9]/i)) {
+            --j;
+        } else if (s[i] === s[j]) {
+            ++i; --j
+        } else {
+            return false
+        }
+    }
+
+    return true
+}
+
+console.log(isValidPalindrom(", a man a plan,  a canal: panama"));
+console.log(isValidPalindrom("abcd"))
